973f59610e82a83b2064989b15c85b5b25255ddf
Myron <dubtwee@gmail.com>
2019-11-17 01:42:38 -0600

    hooking up add color buttons so that they update an array with the appropriate color item

--- client/scripts/components/Swatches/SwatchesController.js
+++ client/scripts/components/Swatches/SwatchesController.js
@@ -10,8 +10,32 @@ class SwatchesController {
     // ------------------------------
 
     _init() {
+        this._swatches = [];
         console.log('initialized SwatchesController');
         this._view = new SwatchesView(this);
+    }
+
+    //
+    // PUBLIC METHODS
+    // ------------------------------
+
+    addSwatch(color) {
+        const firstChar = color.charAt(0);
+        const label = color.replace(firstChar, firstChar.toUpperCase());
+
+        this._swatches.push({
+            label,
+            color,
+        });
+        console.log('addSwatch color: ', color);
+    }
+
+    //
+    // GETTERS & SETTERS
+    // ------------------------------
+
+    get swatches() {
+        return this._swatches;
     }
 }
 
--- client/scripts/components/Swatches/SwatchesView.js
+++ client/scripts/components/Swatches/SwatchesView.js
@@ -22,6 +22,31 @@ class SwatchesView {
 
     _createHandlers() {
         this._$fieldLayout = $('.js-field-layout').on('change', this._onChangeLayout.bind(this));
+        this._$addSwatchBtns = $('.js-btn-add-swatch').on('click', this._onClickAddSwatch.bind(this));
+    }
+
+    _render() {
+        const swatchList = this._controller.swatches;
+        console.log('swatchList: ', swatchList);
+
+        this._$swatchesContr.empty();
+        for (let i = 0; i < swatchList.length; i++) {
+            const indvSwatch = swatchList[i];
+
+            this._$swatchesContr.append(`
+                <li>
+                    <div class="colorCard">
+                        <div class="colorCard-swatch colorCard-swatch_${indvSwatch.color}"></div>
+                        <div class="colorCard-label">
+                            <h4>${indvSwatch.label}</h4>
+                        </div>
+                        <div class="colorCard-action">
+                            <button class="btn js-color-card-delete" data-swatch-id="${i}">Delete</button>
+                        </div>
+                    </div>
+                </li>
+            `);
+        }
     }
 
     //
@@ -40,7 +65,11 @@ class SwatchesView {
         this._$swatchesContr.attr('class', styling);
     }
 
-
+    _onClickAddSwatch(event) {
+        const newColor = event.target.dataset.color;
+        this._controller.addSwatch(newColor);
+        this._render();
+    }
 }
 
 export default SwatchesView;
